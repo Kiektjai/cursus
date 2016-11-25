08cf7869d6c87454d0c20ed5196cbb292a33f35a
Kiektjai <stanleyman1990@hotmail.com>
2016-11-25 12:25:59 +0100

    Simplified code

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -8,36 +8,32 @@ class Validator{
     $errors = [];
 
     foreach ($validation_data as $name => $value) {
-      if(isset($_REQUEST[$name])){
 
-        $rules = explode("|", $value);
+      $rules = explode("|", $value);
 
-        foreach ($rules as $rule) {
-          $exploded = explode(":", $rule);
+      foreach ($rules as $rule) {
+        $exploded = explode(":", $rule);
 
-          switch ($exploded[0]){
-            case 'min':
-              $min = $exploded[1];
-              if(Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false){
-                $errors[] = $name . " must be at least " . $min . " characters long.";
-              }
-              break;
-            case 'email':
-              if(Valid::email()->Validate($_REQUEST[$name]) == false){
-                $errors[] = $name . " must be a valid email addres";
-              }
-              break;
-            case 'equalTo':
-              if(Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false){
-                $errors[] = "Value does not match verification value!";
-              }
-              break;
-            default:
-              // do nothing
-          }
+        switch ($exploded[0]){
+          case 'min':
+            $min = $exploded[1];
+            if(Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false){
+              $errors[] = $name . " must be at least " . $min . " characters long.";
+            }
+            break;
+          case 'email':
+            if(Valid::email()->Validate($_REQUEST[$name]) == false){
+              $errors[] = $name . " must be a valid email addres";
+            }
+            break;
+          case 'equalTo':
+            if(Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false){
+              $errors[] = "Value does not match verification value!";
+            }
+            break;
+          default:
+            $errors[] = "No value found.";
         }
-      }else{
-        $errors[] = "No value found.";
       }
     }
     return $errors;
